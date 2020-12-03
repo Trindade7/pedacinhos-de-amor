234ca8834594d4a77fe18448c58dc37ce703ff96
José Trindade <trindade.jose77@gmail.com>
2020-12-03 11:31:26 +0000

    added categories assets

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,9 +8,14 @@ import { BannerModel, BannerSimpleModel } from '@app/core/models/banner-model';
 export class HomeComponent implements OnInit {
   events: BannerSimpleModel[] = [
     {
-      name: 'Casamentos',
-      color: '#6699ff',
-      imageUrl: '../../../assets/images/cake-categories/wed22.png'
+      name: 'Natal',
+      color: '#4AB9D4',
+      imageUrl: '../../../assets/images/cake-categories/xmas.png'
+    },
+    {
+      name: 'Únicos',
+      color: '#00a99e',
+      imageUrl: '../../../assets/images/cake-categories/uniquel.png'
     },
     {
       name: 'Aniversários',
@@ -18,24 +23,19 @@ export class HomeComponent implements OnInit {
       imageUrl: '../../../assets/images/cake-categories/birthdays.png'
     },
     {
-      name: 'Natal',
-      color: '#4AB9D4',
-      imageUrl: '../../../assets/images/cake-categories/xmas.png'
-    },
-    {
       name: 'Ano Novo',
       color: '#edeaf5',
       imageUrl: '../../../assets/images/cake-categories/new-year.png'
     },
     {
+      name: 'Casamentos',
+      color: '#6699ff',
+      imageUrl: '../../../assets/images/cake-categories/wed22.png'
+    },
+    {
       name: 'Páscoa',
       color: '#F9F0E2',
       imageUrl: '../../../assets/images/cake-categories/easter.png'
-    },
-    {
-      name: 'Outros',
-      color: '#FFCCFF',
-      imageUrl: '../../../assets/images/cake-categories/wed3.png'
     },
   ];
 
